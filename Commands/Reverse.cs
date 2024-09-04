42eb27acf5fb87ca6a824f79ed2c9e1a44f748d3
Daniil Savchenko <95571784+LigindaLeg@users.noreply.github.com>
2024-09-04 23:56:30 +0400

    Update
    Removed RP-Mode
    Fixed bugs with cheater command
    Added AutoUpdater
    Fixed SCP:SL bug with pause on end round

---
+++ Commands/Reverse.cs
@@ -0,0 +1,81 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.Permissions.Extensions;
+using System;
+using UnityEngine;
+
+namespace MultiTools.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class Reverse : ICommand
+    {
+        public string Command { get; } = "reverse";
+
+        public string[] Aliases { get; } = {};
+
+        public string Description { get; } = "Reverse player.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(sender);
+            if (!sender.CheckPermission("mt.reverse"))
+            {
+                response = "You do not have permission to use this command!";
+                return false;
+            }
+            else if (arguments.Count < 1)
+            {
+                if (player.Scale != new Vector3(-1, -1, -1))
+                {
+                    player.Scale = new Vector3(-1, -1, -1);
+                    response = "Reversed!";
+                    return true;
+                }
+                else if (player.Scale == new Vector3(-1, -1, -1))
+                {
+                    player.Scale = new Vector3(1, 1, 1);
+                    response = "Reversed!";
+                    return true;
+                }
+            }
+            else if (arguments.At(0) == player.Id.ToString())
+            {
+                if (player.Scale != new Vector3(player.Scale.x, -1, player.Scale.z))
+                {
+                    player.Scale = new Vector3(player.Scale.x, -1, player.Scale.z);
+                    response = "Reversed!";
+                    return true;
+                }
+                else if (player.Scale == new Vector3(player.Scale.x, -1, player.Scale.z))
+                {
+                    player.Scale = new Vector3(player.Scale.x, 1, player.Scale.z);
+                    response = "Reversed!";
+                    return true;
+                }
+            }
+            else if (arguments.At(0) != player.Id.ToString())
+            {
+                player = Player.Get(arguments.At(0));
+                if (player == null)
+                {
+                    response = $"Player with ID {arguments.At(0)} not found";
+                    return false;
+                }
+                else if (player.Scale != new Vector3(player.Scale.x, -1, player.Scale.z))
+                {
+                    player.Scale = new Vector3 (player.Scale.x, -1, player.Scale.z);
+                    response = $"Reversed {player.Nickname}!";
+                    return true;
+                }
+                else if (player.Scale == new Vector3(player.Scale.x, -1, player.Scale.z))
+                {
+                    player.Scale = new Vector3(player.Scale.x, 1, player.Scale.z);
+                    response = $"Reversed {player.Nickname}!";
+                    return true;
+                }
+            }
+            response = "Using: reverse [ID]";
+            return true;
+        }
+    }
+}
---
+++ Config.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Exiled.API.Interfaces;
+using PlayerRoles;
+
+namespace MultiTools
+{
+    public class Config : IConfig
+    {
+        [Description("Is the plugin enabled?")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Are debug messages displayed?")]
+        public bool Debug { get; set; } = false;
+        [Description("How much time does a cheater have before being banned?")]
+        public float CheatTime { get; set; } = 30;
+        [Description("Cheat Ban Reason?")]
+        public string BanReason { get; set; } = "You are banned for cheating [MultiTools]";
+
+        [Description("Discord Webhook to Ban-Notify?")]
+        public string WebhookNotifyBan { get; set; } = "Paste your webhook here";
+
+        [Description("Discord Message Template?")]
+        public string DSMessage { get; set; } = "{bantime} \n```html\n<Выдал:> {admin} \n<Нарушитель:> {bad} \n<Причина:> {reason} \n```";
+
+    }
+}

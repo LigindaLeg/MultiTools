0fa7540acb019006937c854f36f311f6d2c200e8
Daniil Savchenko <95571784+LigindaLeg@users.noreply.github.com>
2025-02-09 19:39:58 +0400

    Add files via upload

--- MultiTools/Plugin.cs
+++ MultiTools/Plugin.cs
@@ -10,7 +10,7 @@ namespace MultiTools
         public override string Name => "MultiTools";
         public override string Author => "Liginda";
         public override Version Version => new Version(1, 1, 1);
-        public override Version RequiredExiledVersion => new Version(8, 9, 6);
+        public override Version RequiredExiledVersion => new Version(9,0,0);
         public override string Prefix => "MultiTools";
 
         public static Plugin Instance;
